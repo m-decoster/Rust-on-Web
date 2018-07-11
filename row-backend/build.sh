f01e0a4f2a98ad5e7132fd8ac1cc652cb9dfd2c9
Mathieu De Coster <mth.decoster@gmail.com>
2018-07-11 21:16:06 +0200

    Fix release builds

--- row-backend/build.sh
+++ row-backend/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-cargo +nightly build --target wasm32-unknown-unknown
+if [ ${ROW_MODE} == "debug" ]; then
+    cargo +nightly build --target wasm32-unknown-unknown
+else
+    cargo +nightly build --target wasm32-unknown-unknown --release
+fi
 if [ $? == 0 ]; then
     wasm-bindgen target/wasm32-unknown-unknown/${ROW_MODE}/row_backend.wasm --out-dir ../wasm_generated
     if [ $? == 0 ]; then
